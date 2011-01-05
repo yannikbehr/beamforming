42c3d4c6721ecd0c84f58685b95c742cdd3bbfd8
yannik <yannik@e8761022-d16b-44b9-b4d4-61dd033994a1>
2011-01-05 01:58:21 +0000

    adding polar plot
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/proc-scripts@1309 e8761022-d16b-44b9-b4d4-61dd033994a1

--- obspy_2D_beam/fk_start.py
+++ obspy_2D_beam/fk_start.py
@@ -1,10 +1,16 @@
 from obspy.core import UTCDateTime, Stream, read
 from obspy.signal.array_analysis import sonic
 from obspy.signal import cornFreq2Paz
+from obspy.signal.util import nextpow2
 import glob
 from obspy.core.util import AttribDict
+import pickle
 
-if 0:
+data = 0
+beam = 0
+polarplt = 0
+
+if data:
     #
     # Load data
     fl = glob.glob('data/*.SAC')
@@ -18,25 +24,64 @@ if 0:
         nstats.longitude = tr.stats.sac.stlo
         nstats.elevation = tr.stats.sac.stel/1000.
         tr.stats['coordinates'] = nstats
-        tr.trim(ts,ts+3600)
+        tr.trim(ts,ts+360)
         sts += s
+    f = open('start.dump','w')
+    pickle.dump(sts,f)
+    f.close()
 
-
-if 0:
+if beam:
+    if not data:
+        f = open('start.dump')
+        sts = pickle.load(f)
+    
     #
     # Execute sonic
+    ts = sts[0].stats.starttime
+    win_len = 36.0
+    sll_x = -.5
+    slm_x = .5
+    sll_y = -.5
+    slm_y = .5
+    sl_s = .01
     kwargs = dict(
             # slowness grid: X min, X max, Y min, Y max, Slow Step
-            sll_x=-.5, slm_x=.5, sll_y=-.5, slm_y=.5, sl_s=0.01,
+            sll_x=sll_x, slm_x=slm_x, sll_y=sll_y, slm_y=slm_y, sl_s=sl_s,
             # sliding window propertieds
-            win_len=360.0, win_frac=1.,
+            win_len=win_len, win_frac=1.,
             # frequency properties
             frqlow=.1, frqhigh=.2, prewhiten=0,
             # restrict output
             semb_thres=-1e9, vel_thres=-1e9, verbose=True, timestamp='mlabhour',
-            stime=ts, etime=ts+3600)
-    outs = sonic(sts, **kwargs)
+            stime=ts, etime=ts+360)
+    outs, amps = sonic(sts, **kwargs)
 
+if 1:
+    df = sts[0].stats.sampling_rate
+    nsamp = int(win_len * df)
+    nfft = nextpow2(nsamp)
+    f = fftfreq(nfft,1./df)[0:nfft/2]
+    namps = amps.mean(axis=0)
+    temp = squeeze(namps[8,:,:])
+    temp = 10*log10(temp)
+    temp -= temp.max()
+    slow_x = arange(sll_x,slm_x+sl_s,sl_s)
+    slow_y = arange(sll_y,slm_y+sl_s,sl_s)
+    x,y = meshgrid(slow_x,slow_y)
+    azimut = np.arctan2(x, y)[::-1]-pi/2
+    slow = np.sqrt(x ** 2 + y ** 2)
+    fig = figure(figsize=(6, 6))
+    ax = fig.add_subplot(1,1,1,projection='polar')
+    ax.contourf(azimut,slow,temp,100)
+    cmap = cm.get_cmap('jet')
+    ax.contourf(azimut,slow,temp, 100,cmap=cmap,antialiased=True,
+                    linewidths=0.1,linstyles='dotted')
+    ax.set_thetagrids([0,45.,90.,135.,180.,225.,270.,315.],
+                      labels=['90','45','0','315','270','225','180','135'])
+    ax.set_rgrids([0.1,0.2,0.3,0.4,0.5],labels=['0.1','0.2','0.3','0.4','0.5'],color='r')
+    ax.grid(True)
+    #ax.plot(-(theta[id1]-90)*pi/180.,slowness[idx][id2],'ko')
+    ax.set_rmax(0.5)
 
 if 0:
     import matplotlib.pyplot as plt
@@ -54,7 +99,7 @@ if 0:
 
 
 
-if 1:
+if polarplt:
     import numpy as np
     import matplotlib.cm as cm
     import matplotlib.pyplot as plt
@@ -102,3 +147,5 @@ if 1:
                      norm=Normalize(vmin=hist.min(), vmax=hist.max()))
         
     plt.show()
+
+
