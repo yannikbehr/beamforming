0f4b38374fdb0750b52a56ba4a46edabf2dd2a45
yannik <yannik@e8761022-d16b-44b9-b4d4-61dd033994a1>
2011-02-01 01:37:57 +0000

    backup
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/proc-scripts@1339 e8761022-d16b-44b9-b4d4-61dd033994a1

--- grid_beamformer.sh
+++ grid_beamformer.sh
@@ -5,7 +5,7 @@
 #$ -q all.q
 #$ -wd /Volumes/Wanaka_01/yannik/taranaki/beamforming/beams
 #$ -m n
-#$ -N vertical_beamforming
+#$ -N horizontal_beamforming
 
 export PROC_SRC=/Users/home/yannik78/dev/proc-scripts_git/
 LIST=/Volumes/Wanaka_01/yannik/taranaki/beamforming/beams/dirlist_april.txt
@@ -13,7 +13,7 @@ ROOT=/Volumes/Wanaka_01/yannik/taranaki/sacfiles/5Hz/2002/Apr
 DIR=$(cat $LIST | head -n $SGE_TASK_ID | tail -n 1)
 INPUT=${ROOT}/${DIR}
 #echo "/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming.py  ${INPUT}"
-#/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming/hbeamforming.py  $INPUT -s
-/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming/beamforming.py  $INPUT -s
+/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming/hbeamforming.py  $INPUT -s
+#/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming/beamforming.py  $INPUT -s
 
 
