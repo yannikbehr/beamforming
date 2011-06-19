e6c751f9819ff5af6bb65bdbf6c21f52efcaa607
yannik <yannik@e8761022-d16b-44b9-b4d4-61dd033994a1>
2011-06-19 21:29:15 +0000

    backup
    git-svn-id: http://svn.geo.vuw.ac.nz/svn/noise_correlation/proc-scripts@1379 e8761022-d16b-44b9-b4d4-61dd033994a1

--- grid_beamformer.sh
+++ grid_beamformer.sh
@@ -15,6 +15,6 @@ LIST=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams/list_
 ROOT=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki
 DIR=$(cat $LIST | head -n $SGE_TASK_ID | tail -n 1)
 INPUT=${ROOT}/${DIR}
-/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming/beamforming.py  $INPUT -s -b --nstat=20
+/usr/local/python2/bin/python2.7 /Users/home/yannik78/dev/proc-scripts_git/beamforming/beamforming.py  $INPUT -s -b -d --nstat=20
 
 
