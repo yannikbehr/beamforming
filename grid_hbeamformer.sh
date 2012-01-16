74d594efb9246d8a6ff4a1dab1affb61e073209b
Yannik Behr <yannik.behr@vuw.ac.nz>
2012-01-17 04:33:55 +1300

    run with extended slowness range

--- grid_hbeamformer.sh
+++ grid_hbeamformer.sh
@@ -3,15 +3,15 @@
 #$ -S /bin/sh
 #$ -t 1-203
 #$ -q all.q
-#$ -wd /Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams
+#$ -wd /Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams_2
 #$ -m n
 #$ -N horizontal_beamforming
-#$ -o /Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams/horizontal_beam.out
-#$ -e /Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams/horizontal_beam.err
+#$ -o /Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams_2/horizontal_beam.out
+#$ -e /Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams_2/horizontal_beam.err
 
 export PROC_SRC=/Users/home/yannik78/dev/proc-scripts_git/
-#LIST=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams/dirlist_april.txt
-LIST=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams/list_all_months.txt
+#LIST=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams_2/dirlist_april.txt
+LIST=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki/beamforming/beams_2/list_all_months.txt
 ROOT=/Volumes/GeoPhysics_05/users-data/yannik78/taranaki/
 DIR=$(cat $LIST | head -n $SGE_TASK_ID | tail -n 1)
 INPUT=${ROOT}/${DIR}
